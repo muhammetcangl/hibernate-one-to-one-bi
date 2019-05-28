537e22e60f62d49e8a784fc875fe4bd306c4d444
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-05-28 20:01:21 +0300

    Deleting instructorDetail.

--- src/com/mcg/hibernate/demo/DeleteInstructorDetailDemo.java
+++ src/com/mcg/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class GetInstructorDetailDemo {
+public class DeleteInstructorDetailDemo {
 
     public static void main(String[] args) {
 
@@ -30,6 +30,10 @@ public class GetInstructorDetailDemo {
             System.out.println("instructorDetail: " + instructorDetail);
 
             System.out.println("the associated instructor: " + instructorDetail.getInstructor());
+
+            System.out.println("Deleting instructorDetail: " + instructorDetail);
+
+            session.delete(instructorDetail);
 
             // commit transaction
             session.getTransaction().commit();
